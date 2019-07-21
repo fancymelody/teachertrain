a0bd005994d3a3dce68ed30c0832636e620c0c59
fancymelody <2769501950@qq.com>
2019-07-21 14:54:20 +0800

    文章待阅读

--- src/components/teachertrain/mystudy/articlestudying.vue
+++ src/components/teachertrain/mystudy/articlestudying.vue
@@ -4,12 +4,12 @@
             <ul class="tabitemlist">
                 <li class="item coursestudying">
                     <a class="tabword">
-                        <router-link to="/teachertrain/acticletudying">待阅读</router-link>
+                        <router-link to="/teachertrain/articlestudying">待阅读</router-link>
                     </a>
                 </li>
                 <li class="item coursestudied">
                     <a class="tabword">
-                        <router-link to="/teachertrain/acticlestudied">已阅读</router-link>
+                        <router-link to="/teachertrain/articlestudied">已阅读</router-link>
                     </a>
                 </li>
             </ul>
@@ -17,38 +17,43 @@
         <div class="acticlecontent">
             <div class="left">
                 <div class="articlemain">
-                    <div class="articleitem">
+                    <div class="articleitem" v-for="(item,index) in articles">
                         <div class="articleitemdetail">
                             <div class="title">
                                 <h2>
                                     <a href="" target="_blank" style="color:#000;" class="titleword">
-                                        <span>{{title}}</span>
+                                        <span>{{item.title}}</span>
                                     </a>
                                 </h2>
                             </div>
                             <div class="abstract">
-                                青少年的心理特点有别于成年人,其心理发展中存在着几对矛盾:自我意识与社会角色的矛盾,理想我与现实我的矛盾
+                                {{item.abstract}}
                             </div>
                             <div class="btnbox">
-                                <img <button class="btn"><span>开始阅读</span></button>
+                                <img :src="item.imgSrc" class="headimage">
+                                <span>{{item.label}}|{{item.time}}</span>
+                                <button class="btn"><span>开始阅读</span></button>
                             </div>
-                        </div>
-                    </div>
-                    <div class="articleitem">
-                        <div class="articleitemdetail">
-
-                        </div>
-                    </div>
-                    <div class="articleitem">
-                        <div class="articleitemdetail">
-
                         </div>
                     </div>
                 </div>
             </div>
             <div class="right">
                 <div class="recommend">
-                    ffff ddddddddddddddddddddddddddddddddddddddddddddddd
+                    <h3>
+                        <span class="line"></span>
+                        <span class="txt">今日推荐</span>
+                    </h3>
+                    <div class="recommendmain">
+                        <div class="recommenditem" v-for="(item,index) in articles">
+                            <div class="recommend_title">
+                                <a href="" target="_blank" style="color:#000;" class="recommendword">
+                                    <span>{{item.title}}</span>
+                                </a>
+                            </div>
+                            <span class="recommendtime">{{item.label}}|{{item.time}}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -60,34 +65,41 @@
         name: 'articlestudying',
         data() {
             return {
-                title: "浅谈青少年的心理特征与心理健康教育",
-                abstract: "青少年的心理特点有别于成年人,其心理发展中存在着几对矛盾:自我意识与社会角色的矛盾,理想我与现实我的矛盾",
-                imgSrc: this.$store.state.url + 'teachertrain/video/class.jpg',
                 articles: [
                     {
                         title: "浅谈青少年的心理特征与心理健康教育",
-                        abstract: "青少年的心理特点有别于成年人,其心理发展中存在着几对矛盾:自我意识与社会角色的矛盾,理想我与现实我的矛盾",
+                        abstract: "青少年的心理特点有别于成年人 ,其心理发展中存在着几对矛盾 :自我意识与社会角色的矛盾 ,理想我与现实我的矛盾 ,情感波动性与内隐性的矛盾 ,性机能成熟与性心理相对幼稚的矛盾等。针对青少年的心理特点 ,青少年心理健康教育的主要内容应包括 :环境适应教育 ,培养良好性情 ,正确认识自己等。",
+                        imgSrc: this.$store.state.url + 'teachertrain/article/headimage.jpg',
+                        label: "青少年心理学",
+                        time: "2019-1-1",
                     },
                     {
-                        progressvalue: 82.4,
-                        imgSrc: this.$store.state.url + 'teachertrain/video/class.jpg',
-                        // imgSrc:this.$store.state.url + 'teachertrain/sidebar/headsculpture.jpg',
-                        name: "人文科技艺术专题",
+                        title: "浅谈青少年的心理特征与心理健康教育",
+                        abstract: "青少年的心理特点有别于成年人 ,其心理发展中存在着几对矛盾 :自我意识与社会角色的矛盾 ,理想我与现实我的矛盾 ,情感波动性与内隐性的矛盾 ,性机能成熟与性心理相对幼稚的矛盾等。针对青少年的心理特点 ,青少年心理健康教育的主要内容应包括 :环境适应教育 ,培养良好性情 ,正确认识自己等。",
+                        imgSrc: this.$store.state.url + 'teachertrain/article/headimage.jpg',
+                        label: "青少年心理学",
+                        time: "2019-1-1",
                     },
                     {
-                        progressvalue: 82.4,
-                        imgSrc: this.$store.state.url + 'teachertrain/video/class.jpg',
-                        name: "人文科技艺术专题",
+                        title: "浅谈青少年的心理特征与心理健康教育",
+                        abstract: "青少年的心理特点有别于成年人 ,其心理发展中存在着几对矛盾 :自我意识与社会角色的矛盾 ,理想我与现实我的矛盾 ,情感波动性与内隐性的矛盾 ,性机能成熟与性心理相对幼稚的矛盾等。针对青少年的心理特点 ,青少年心理健康教育的主要内容应包括 :环境适应教育 ,培养良好性情 ,正确认识自己等。",
+                        imgSrc: this.$store.state.url + 'teachertrain/article/headimage.jpg',
+                        label: "青少年心理学",
+                        time: "2019-1-1",
                     },
                     {
-                        progressvalue: 82.4,
-                        imgSrc: this.$store.state.url + 'teachertrain/video/class.jpg',
-                        name: "人文科技艺术专题",
+                        title: "浅谈青少年的心理特征与心理健康教育",
+                        abstract: "青少年的心理特点有别于成年人 ,其心理发展中存在着几对矛盾 :自我意识与社会角色的矛盾 ,理想我与现实我的矛盾 ,情感波动性与内隐性的矛盾 ,性机能成熟与性心理相对幼稚的矛盾等。针对青少年的心理特点 ,青少年心理健康教育的主要内容应包括 :环境适应教育 ,培养良好性情 ,正确认识自己等。",
+                        imgSrc: this.$store.state.url + 'teachertrain/article/headimage.jpg',
+                        label: "青少年心理学",
+                        time: "2019-1-1",
                     },
                     {
-                        progressvalue: 82.4,
-                        imgSrc: this.$store.state.url + 'teachertrain/video/class.jpg',
-                        name: "人文科技艺术专题",
+                        title: "浅谈青少年的心理特征与心理健康教育",
+                        abstract: "青少年的心理特点有别于成年人 ,其心理发展中存在着几对矛盾 :自我意识与社会角色的矛盾 ,理想我与现实我的矛盾 ,情感波动性与内隐性的矛盾 ,性机能成熟与性心理相对幼稚的矛盾等。针对青少年的心理特点 ,青少年心理健康教育的主要内容应包括 :环境适应教育 ,培养良好性情 ,正确认识自己等。",
+                        imgSrc: this.$store.state.url + 'teachertrain/article/headimage.jpg',
+                        label: "青少年心理学",
+                        time: "2019-1-1",
                     }
                 ],
             }
@@ -177,14 +189,15 @@
         float: right;
         width: 20%;
         height: 100%;
-        background: rgb(133, 207, 133);
+        border: 1px solid #000;
+        /* background: rgb(133, 207, 133); */
     }
 
     .articlemain {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        border: 1px solid #000;
+        /* border: 1px solid #000; */
     }
 
     .articleitem {
@@ -194,21 +207,26 @@
         display: flex;
         flex-direction: column;
         margin: 0px 5px;
-        border: 1px solid rgb(241, 6, 6);
+        /* border: 1px solid rgb(241, 6, 6); */
         background: #fff;
-        /* border-bottom: 1px solid #f4f4f4; */
+        border-bottom: 1px solid #f5f3f3;
         box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.04);
     }
 
-    .articleitemdetail {
-        border: 1px solid slateblue;
+    .articleitem:hover {
+        background: #f5f6f7;
     }
+
+    /* .articleitemdetail {
+        border: 1px solid slateblue;
+    } */
 
     h2 {
         font-size: 24px;
         font-weight: bold;
         line-height: 24px;
         height: 24px;
+        max-height: 24px;
         overflow: hidden;
         white-space: nowrap;
     }
@@ -222,5 +240,76 @@
         color: #8a8a8a;
         font-size: 18px;
         line-height: 24px;
+        max-height: 24px;
+        overflow: hidden;
+    }
+
+    .headimage {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        object-fit: cover;
+        overflow: hidden;
+    }
+
+    .btn {
+        float: right;
+        margin-top: 10px;
+        margin-right: 10px;
+        border-radius: 5px;
+        background-color: #51c79f;
+        color: #fff;
+    }
+
+    .line {
+        display: block;
+        float: left;
+        width: 3px;
+        height: 25px;
+        background: #cf2730;
+        overflow: hidden;
+        margin-left: 15px;
+    }
+
+    .txt {
+        margin-left: 5px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .recommendmain {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        border: 1px solid #000;
+    }
+
+    .recommenditem {
+        padding: 10px 10px;
+        width: 100%;
+        height: 80px;
+        display: flex;
+        flex-direction: column;
+        margin: 0px 5px;
+        background: #fff;
+        border-bottom: 1px solid #f5f3f3;
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.04);
+    }
+
+    .recommend_title {
+        font-size: 16px;
+        line-height: 24px;
+        height: 24px;
+        max-height: 24px;
+        overflow: hidden;
+    }
+
+    .recommendword span:hover {
+        color: red;
+    }
+
+    .recommendtime {
+        color: #858585;
+        margin-top: 10px;
     }
 </style>
