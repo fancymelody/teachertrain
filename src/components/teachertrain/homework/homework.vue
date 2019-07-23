a00b63b548e24e6b245af5f3578b6c6bef2d8a3e
Wahaha2333 <1095182430@qq.com>
2019-07-23 16:07:17 +0800

    完善作业模块

--- src/components/teachertrain/homework/homework.vue
+++ src/components/teachertrain/homework/homework.vue
@@ -2,7 +2,7 @@
     <div class="test">
         <div class="tabbar">
             <ul class="breadcrumb">
-                <li v-for="item in crumb"><a href="#" >{{item}}</a></li>
+                <li v-for="item in crumb" :key="item.id"><a href="#" >{{item}}</a></li>
                 <li class="active">{{title}}</li>
             </ul>
         </div>
@@ -19,26 +19,29 @@
                         <div class="top">
                             <table class="table table-striped table-bordered table-hover">
                                 <tbody>
-                                    <tr v-for="item in title">
+                                    <tr v-for="item in titles" :key="item.id">
                                         <th>{{item.name}}</th>
                                         <td>{{item.content}}</td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
-                        <div class="question">
-                            <div class="que" v-for="(q,n) in question">
-                                <div class="topic">
-                                    <h4><strong>题目{{++n}}</strong>&nbsp({{q.score}}分)</h4>
-                                    <h4>{{q.problem}}</h4>
-                                </div>
-                                <div class="select">
-                                    <div class="option"  v-for="(value,key) in q.select">
-                                            <label><input name="radio" type="radio">{{key}}.{{value}}</label>
+                        <form action="" @submit.prevent="submit">
+                            <div class="question">
+                                <div class="que" v-for="(q,index) in question" :key="q.id">
+                                    <div class="topic">
+                                        <h4><strong>题目{{++index}}</strong>&nbsp({{q.score}}分)</h4>
+                                        <h4>{{q.problem}}</h4>
+                                    </div>
+                                    <div class="select">
+                                        <div class="option"  v-for="(value,key) in q.select" :key="value.id">
+                                                <input :name="q.name" type="radio" value="key" v-model="checkedValue[index]" @compare="get_radio_value(index)"><label>{{key}}.{{value}}</label>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div> 
+                            </div> 
+                            <button type="submit" class="btn btn-info" value="提交">提交</button>
+                        </form>
                     </div>
                 </div>
                 <div class="sidebar col-lg-3">
@@ -49,14 +52,16 @@
                             </h3>
                         </div>
                         <div class="number">
-                            <span v-for="num in n">1</span>
+                            <span class="num" v-for="num in 5">{{num}}</span>
                         </div>
+                        <button type="submit" class="btn btn-info" @click="submit">提交</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 <script>
     export default{
         data(){
@@ -64,7 +69,7 @@
                 crumb:['我的作业','FLutter',],
                 title:'第一章综合测试',
                 back:this.$store.state.url+"homework/back.png",
-                title:[
+                titles:[
                     {
                         name:"名称",
                         content:"Flutter"
@@ -80,6 +85,7 @@
                 ],
                 question:[
                     {
+                        name:'t1',
                         score:"20",
                         problem:"E-R图是数据库设计的工具之一，它适用于建立数据库的().",
                         select:{
@@ -90,6 +96,7 @@
                         }
                     },
                     {
+                        name:'t2',
                         score:"20",
                         problem:"E-R图是数据库设计的工具之一，它适用于建立数据库的().",
                         select:{
@@ -100,6 +107,7 @@
                         }
                     },
                     {
+                        name:'t3',
                         score:"20",
                         problem:"E-R图是数据库设计的工具之一，它适用于建立数据库的().",
                         select:{
@@ -109,9 +117,32 @@
                             D:"概念模型"
                         }
                     }
-                ]
+                ],
+                all_list: [], //答案数组
+                checkedValue: [],
+                num:[1,2,3,4,5],
+                
+            };
+        },
+        methods:{
+            submit:function(){
+                for(var i=0;i<this.all_list.length;i++){
+                    if (this.all_list[i] == "" || typeof this.all_list[i] === "undefined") {
+                    this.all_list.splice(i, 1);
+                    }
+                }
+                if(this.all_list.length != this.question.length){
+                    alert("还有"+this.question.length-this.all_list.length+"未做，确认提交？");
+                }else{
+                    alert("已做完，确认提交？");
+                    this.$emit('',this.all_list)
+                }
+            },
+            get_radio_value:function(index){
+                this.all_list[index]==this.checkedValue[index];
             }
         }
+        
     }
 </script>
 <style scoped>
@@ -133,23 +164,28 @@
     }
     .page{
         width:80%;
-        height: 1024px;
+        height: auto;
         margin-top:20px;
         margin-left: 10%;
+        margin-bottom: 20px;
+        
     }
     .content{
-        height: 100%;
+        height: auto;
         background:#FFF;
         width: 73%;
         border: 2px solid rgba(236,237,240,1);
         border-radius: 5px;
+        box-shadow: 0 5px 10px rgba(0,0,0,0.2);
+        
     }
     .sidebar{
-        height: 100%;
+        height: auto;
         background:#FFF;
         float: right;
         border: 2px solid rgba(236,237,240,1);
         border-radius: 5px;
+        box-shadow: 0 5px 10px rgba(0,0,0,0.2);
     }
     .back{
         margin-top:20px;
@@ -188,7 +224,7 @@
     .block .header{
     font-weight: 300;
     border-bottom: 3px solid #ddd;
-    margin: 0 0 10px 0;
+    margin: 0 0 20px 0;
     }
     .header ::before{
         margin-right:2px;
@@ -203,5 +239,25 @@
         width: 150px;
         position: relative;
         top:13px;
+    }
+    .number span{
+        display:inline-block;
+        border:solid;
+        border: 1px solid rgba(236,237,240,1);
+        color: #024891;
+        background:#d9edf7;
+        border-radius: 4px;
+        margin-left: 20px;
+        width: 30px;
+        text-align: center;
+        font-size: 18px;
+        font-weight: 600;
+        box-shadow: 0 5px 10px rgba(204, 199, 199, 0.2);
+
+    }
+    .btn{
+        margin-top:40px;
+        margin-bottom: 20px;
+        float: right;
     }
 </style>
